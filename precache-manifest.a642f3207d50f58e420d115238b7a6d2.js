eb2666ff1365b8f4b05e527265cd3a17103c3307
571733 <jindrebo@hotmail.no>
2019-01-30 19:06:29 +0100

    Updates

---
+++ precache-manifest.a642f3207d50f58e420d115238b7a6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a66ac1f97ff004b272d2",
+    "url": "/workshop/static/js/runtime~main.a66ac1f9.js"
+  },
+  {
+    "revision": "af7a71851d2e84a41fb1",
+    "url": "/workshop/static/js/main.af7a7185.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "af7a71851d2e84a41fb1",
+    "url": "/workshop/static/css/main.54abd51f.chunk.css"
+  },
+  {
+    "revision": "e778fe170b4caf23aa49986083d9bf35",
+    "url": "/workshop/index.html"
+  }
+];
